c149fa1cffa39206f7924a837ec003b11184c7f6
LohurLaura <lohurlaura@gmail.com>
2023-12-11 13:06:40 +0200

    Simple example of inheritance.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Dogs firstDog = new Dogs();
+        firstDog.name = "Mikey";
         System.out.println("Dog: ");
         firstDog.animalMoves();
         
         Cats firstCat = new Cats();
+        firstCat.color = "Red";
         System.out.println("Cat: ");
         firstCat.animalMoves();
     }
